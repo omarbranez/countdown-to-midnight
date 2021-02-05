5523386cc044ae629f6de337d6e749c1cfbbe0f2
Omar Branez <omar.branez@gmail.com>
2021-02-05 03:40:28 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(time_to_midnight)
+    while time_to_midnight > 0
+        puts "#{time_to_midnight} SECOND(S)!"
+        time_to_midnight -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(time_to_midnight)
+    while time_to_midnight > 0
+        puts "#{time_to_midnight} SECOND(S)!"
+        time_to_midnight -= 1
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!"
 end
